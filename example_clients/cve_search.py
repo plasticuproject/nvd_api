8f128d22c57f491e661681be422e990d453e2a09
plasticuproject <19690649+plasticuproject@users.noreply.github.com>
2020-02-27 11:19:40 -0500

    Added example python client

---
+++ example_clients/cve_search.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+#MIT License
+#Copyright (C) 2020 plasticuproject@pm.me
+# 'pip install requests' to use
+
+import requests
+from sys import argv
+from json.decoder import JSONDecodeError
+
+# API address
+url = 'https://plasticuproject.pythonanywhere.com/nvd-api/v1/'
+
+
+# The main app funtion
+def search(*args):
+
+    cves = {}
+
+    # Prints help/usage info
+    if len(args) == 1 or args[1] == '-h' or args[1] == '--help':
+        print('''\nSearch CVE records by ID, YEAR and/or KEYWORD. Prints ID and description only.\n
+        USAGE:
+        ./cve_search.py <CVE-ID>
+        ./cve_serach.py <year> (keyword)
+        ./cve_search.py all (keyword)
+        ./cve_search.py recent (keyword)
+        ./cve_search.py modified (keyword)
+        ''' + '\n')
+        quit()
+
+    # Adds CVE ID and description to cves dictionary from results matching CVE-ID queried
+    elif len(args) == 2 and args[1].startswith('cve') == True or args[1].startswith('CVE') == True:
+        cve = args[1]
+        res = requests.get(url + cve)
+        cves[res.json()['cve']['CVE_data_meta']['ID']] = res.json()['cve']['description']['description_data'][0]['value']
+
+    # Adds CVE ID/s and descriptions/s to cves dictionary from results list if CVEs are found matching criteria
+    else:
+        date = args[1]
+        year = 'year/'
+        keyword = ' '.join(args[2:])
+        if date == 'all' or date == 'recent' or date == 'modified':
+            year = date
+            date = ''
+        res = requests.get(url + year + date + '?keyword=' + keyword)
+        for i in res.json():
+            cves[i['cve']['CVE_data_meta']['ID']] = i['cve']['description']['description_data'][0]['value']
+
+    # Prints if no results are found and cves dictionary is empty
+    if len(cves) == 0:
+        print('No results found.')
+        quit()
+
+    # Prints CVE ID/s and description/s from cves dictionary, if any
+    for k, v in cves.items():
+        print('\n' + k)
+        print(v + '\n')
+
+
+if __name__ == '__main__':
+
+    # Run search function and catch all errors and exceptions
+    try:
+        search(*argv)
+
+    except (KeyError, TypeError):
+        user_in = ' '.join(argv[1:])
+        print('Did not understand your request for: ' + user_in)
+        quit()
+
+    except JSONDecodeError:
+        print('NETWORK ERROR: Please check your request or try again later.')
+        quit()
+
+    except KeyboardInterrupt:
+        quit()
+
+
